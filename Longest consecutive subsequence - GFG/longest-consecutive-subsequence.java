4dc1b5a57bb3eafc924bfa19802a3de82f2df93d
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-05-04 11:23:40 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	    HashMap<Integer,Boolean> memo = new HashMap<>();
+	    for(int val: arr){
+	        memo.put(val,true);
+	    }
+	    for(int val:arr){
+	        if(memo.containsKey(val-1))
+	        {
+	            memo.put(val,false);
+	        }
+	    }
+	    int maxl=0;
+	    for(int val:arr){
+	        int len=0;
+	        if(memo.get(val)==true){
+	            len++;
+	            while(memo.containsKey(val+len)){
+	                len++;
+	            }
+	            maxl = Math.max(maxl,len);
+	        }
+	    }
+	    return maxl;
+	}
+}
