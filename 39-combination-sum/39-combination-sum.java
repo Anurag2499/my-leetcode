1e9e9e94f471554f455f0e03ae1870eda5652e31
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-05-27 12:11:58 +0530

    Time: 3 ms (91.36%), Space: 44.6 MB (61.22%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        combinations(candidates, target, 0,new ArrayList<Integer>(),ans);
+        return ans;
+    }
+    private void combinations(int[] candidates, int target, int ci,ArrayList<Integer> currentsubset, List<List<Integer>> ans)
+    {
+        if(target==0)
+        {
+            ans.add(new ArrayList(currentsubset));
+            return;
+        }
+        
+        if(ci>=candidates.length)
+            return ;
+        
+        
+
+        if(target>=candidates[ci])
+        {
+        currentsubset.add(candidates[ci]);
+        combinations(candidates, target-candidates[ci],ci,currentsubset,ans);
+        currentsubset.remove(currentsubset.size()-1);
+        }
+        
+        combinations(candidates, target,ci+1,currentsubset, ans);
+        return ;
+    }
+}
