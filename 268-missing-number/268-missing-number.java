4e71418487ce382074d7ca2cc1582e33f1124c93
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-05-28 11:28:06 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (78.84%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,22 +1,31 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashMap<Integer,Integer> memo = new HashMap<>();
-        for(int i=0;i<=nums.length;i++)
-        {
-            memo.put(i,1);
-        }
+        // HashMap<Integer,Integer> memo = new HashMap<>();
+        // for(int i=0;i<=nums.length;i++)
+        // {
+        //     memo.put(i,1);
+        // }
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(memo.containsKey(nums[i]))
+        //     {
+        //         memo.put(nums[i],0);
+        //     }
+        // }
+        // for(int i=0;i<=nums.length;i++)
+        // {
+        //     if(memo.get(i)==1)
+        //         return i;
+        // }
+        // return 0;
+        
+//         using bit manipulation
+        int ans=0;
+        
         for(int i=0;i<nums.length;i++)
         {
-            if(memo.containsKey(nums[i]))
-            {
-                memo.put(nums[i],0);
-            }
+            ans=ans^nums[i]^(i+1);
         }
-        for(int i=0;i<=nums.length;i++)
-        {
-            if(memo.get(i)==1)
-                return i;
-        }
-        return 0;
+        return ans;
     }
 }
