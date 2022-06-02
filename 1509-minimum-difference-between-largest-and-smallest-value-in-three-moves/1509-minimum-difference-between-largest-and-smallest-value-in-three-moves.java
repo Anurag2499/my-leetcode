18cd1cf6d31728500c75aecb60703254a594ce46
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-06-02 11:01:35 +0530

    Time: 21 ms (43.31%), Space: 65.1 MB (68.56%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -6,14 +6,21 @@ class Solution {
         if(n<=4)
             return 0;
         
-        int p = nums[n-4]-nums[0];
-        int q = nums[n-1]-nums[3];
-        int r = nums[n-3]-nums[1];
-        int s = nums[n-2]-nums[2];
-        int a=(r<s?r:s);
-        int b= (p<q?p:q);
+        int res=Integer.MAX_VALUE;
+        for(int i=0;i<=3;i++)
+        {
+            res=Math.min(res,nums[n-4+i]-nums[i]);
+        }
+        return res;
+        
+//         int p = nums[n-4]-nums[0];
+//         int q = nums[n-1]-nums[3];
+//         int r = nums[n-3]-nums[1];
+//         int s = nums[n-2]-nums[2];
+//         int a=(r<s?r:s);
+//         int b= (p<q?p:q);
             
-        return a<b?a:b;
+//         return a<b?a:b;
         
         
         
