86678f0b956fc1021d95dc0a14c161ec61ea2a2f
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-06-30 16:48:17 +0530

    Time: 3 ms (98.32%), Space: 43.1 MB (85.08%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -5,13 +5,24 @@ class Solution {
         int mid=nums.length/2;
         
         int ans=0;
-        for(int i=0;i<nums.length;i++)
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(i<mid)
+        //         ans+= nums[mid]-nums[i];
+        //     if(i>mid)
+        //         ans+= nums[i]-nums[mid];
+        // }
+        int i=0,j=nums.length-1;
+        while(i<j)
         {
-            if(i<mid)
-                ans+= nums[mid]-nums[i];
-            if(i>mid)
-                ans+= nums[i]-nums[mid];
+            ans+= nums[j]-nums[i];
+            j--;
+            i++;
         }
         return ans;
     }
 }
+// so what we are doing is lets say nums[mid] = C
+//  C-nums[0] + C-nums[1] + C-nums[2]......+ C-nums[mid-1] + nums[mid+1]-C + nums[mid+2]-C +......nums[n-1]-C
+//  so here all the C's will cancel each other
+//  nums[n-1]-nums[0] + nums[n-2]-nums[1]..... nums[mid+1]-nums[mid-1]
