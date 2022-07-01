0ea367310cd14c6fcb4d18ae500422a0b3115821
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-07-01 14:37:58 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (99.00%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -2,11 +2,18 @@ class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         int[] contain = new int[1001];
         
+        
+        //so here index is showing units per boxes and value is showing no of boxes 
+        // we are adding because there can be different no. of boxes which have same units per box , so to add them
         for(int i=0;i<boxTypes.length;i++)
         {
             contain[boxTypes[i][1]]+=boxTypes[i][0];
         }
         
+        
+        
+//         we are iterating from backside to get more units
+//         newunit is the no. of boxes and i is the unit per box
         int ans=0;
         for(int i=1000;i>=0;i--)
         {
