2148dab49108cc957448a92b948294c23680223a
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-09 10:10:37 +0530

    Time: 0 ms (100.00%), Space: 51.6 MB (47.00%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -15,12 +15,16 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null) return null;
+        while(root!=null && root.val!=val){
+            root = root.val>val? root.left : root.right;
+        }
+        return root;
+//         if(root==null) return null;
         
-        if(root.val == val) return root;
+//         if(root.val == val) return root;
         
-        if(root.val > val) 
-            return searchBST(root.left,val);
-        return searchBST(root.right,val);
+//         if(root.val > val) 
+//             return searchBST(root.left,val);
+//         return searchBST(root.right,val);
     }
 }
