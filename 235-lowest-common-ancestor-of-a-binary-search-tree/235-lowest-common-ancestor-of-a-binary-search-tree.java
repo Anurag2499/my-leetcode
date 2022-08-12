3fcc818aa46cdd18362250cf439e741d3931d671
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-12 18:14:33 +0530

    Time: 9 ms (37.36%), Space: 50.1 MB (30.71%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        if(root==null) return null;
+        if(root==p || root==q) return root;
+        
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        
+        if(left==null) return right;
+        else if(right==null) return left;
+        else return root;
+        
+    }
+}
+    
+//         ArrayList<Integer> p1 = new ArrayList<>();
+//         ArrayList<Integer> p2 = new ArrayList<>();
+        
+//         path(root,p,p1);
+//         path(root,q,p2);
+//         int i=0;
+//         TreeNode res = new TreeNode(-1);
+//         while(i<(Math.min(p1.size(),p2.size())))
+//         {
+//             if(p1.get(i)==p2.get(i)) res = new TreeNode(p1.get(i));
+//             if(p1.get(i)!=p2.get(i)) return res;
+//             i++;
+//         }
+//         return res;
+        
+    // }
+//     private Boolean path(TreeNode root, TreeNode p, ArrayList<Integer> p1)
+//     {
+//         if(root==null) return false;
+        
+//         p1.add(root.val);
+        
+//         if(root==p) return true;
+        
+//         if(path(root.left,p,p1) || path(root.right,p,p1))
+//             return true;
+        
+//         p1.remove(p1.size()-1);
+//         return false;
+//     }
+// }
