a8da82dc78fe365625bf31a8796c01f7d1617702
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-18 17:50:52 +0530

    Time: 36 ms (90.85%), Space: 51.4 MB (94.87%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -6,8 +6,6 @@ class Solution {
         {
             if(map.containsKey(arr[i])) map.put(arr[i],map.get(arr[i])+1);
             else map.put(arr[i],1);
-            
-            if(map.get(arr[i])>=n/2) return 1;
         }
         
         ArrayList<Integer> a = new ArrayList<>(map.values());
