d9b7952832e6fcd650da44051f755c060e7bf67e
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-24 14:52:27 +0530

    Time: 3 ms (95.96%), Space: 42.6 MB (88.70%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,29 +1,68 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++)
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i=0;i<nums2.length;i++)
         {
-            int ele = nums1[i];
-            int index=0;
-            for(int j=0;j<nums2.length;j++)
+            while(!st.isEmpty() && nums2[i]>st.peek())
             {
-                if(nums2[j]==ele){
-                    index=j;
-                    break;
-                }
+                map.put(st.peek(),nums2[i]);
+                st.pop();
             }
-            int flag=0;
-            for(int j=index+1;j<nums2.length;j++)
-            {
-                if(nums2[j]>ele)
-                {
-                    ans[i]=nums2[j];
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==0) ans[i]=-1;
+            st.push(nums2[i]);
+        }
+        
+        int[] ans = new int[nums1.length];
+        for(int i =0;i<nums1.length;i++){
+            if(map.containsKey(nums1[i]))
+                ans[i]=map.get(nums1[i]);
+            else
+                ans[i]=-1;
         }
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int[] ans = new int[nums1.length];
+        // for(int i=0;i<nums1.length;i++)
+        // {
+        //     int ele = nums1[i];
+        //     int index=0;
+        //     for(int j=0;j<nums2.length;j++)
+        //     {
+        //         if(nums2[j]==ele){
+        //             index=j;
+        //             break;
+        //         }
+        //     }
+        //     int flag=0;
+        //     for(int j=index+1;j<nums2.length;j++)
+        //     {
+        //         if(nums2[j]>ele)
+        //         {
+        //             ans[i]=nums2[j];
+        //             flag=1;
+        //             break;
+        //         }
+        //     }
+        //     if(flag==0) ans[i]=-1;
+        // }
+        // return ans;
     }
 }
