e1e64dedec35b3e347ecc44e395bcda0fbc4b8be
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-25 20:24:48 +0530

    Time: 11 ms (84.79%), Space: 79.7 MB (16.34%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class NodeValue{
+    int minNode;
+    int maxNode;
+    int sum;
+    NodeValue(int minNode, int maxNode, int sum){
+        this.minNode = minNode;
+        this.maxNode = maxNode;
+        this.sum = sum;
+    }
+}
+class Solution {
+    public NodeValue helper(TreeNode root,int[] maxsum)
+    {
+        if(root==null) 
+            return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE,0);
+        
+        if(root.left==null && root.right==null) {
+            maxsum[0] = Math.max(maxsum[0],root.val);
+            return new NodeValue(root.val,root.val, root.val);
+        }
+        
+        NodeValue left = helper(root.left,maxsum);
+        NodeValue right= helper(root.right,maxsum);
+        
+        if(root.val > left.maxNode && root.val < right.minNode)
+        {
+            maxsum[0] = Math.max(maxsum[0], left.sum + right.sum + root.val );
+            return new NodeValue( Math.min(root.val,left.minNode), Math.max(root.val,right.maxNode), left.sum + right.sum + root.val );
+        }
+        // maxsum[0] = Math.max(maxsum[0], Math.max(left.sum,right.sum));
+        
+        return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+    
+    public int maxSumBST(TreeNode root) {
+        int[] maxsum = new int[1];
+        helper(root,maxsum);
+        int ans = maxsum[0];
+        if(ans>0) return ans;
+        return 0;
+    }
+}
