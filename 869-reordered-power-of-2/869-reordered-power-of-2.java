cd639e08785702f797e76806d8f24093c8608ce4
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-27 22:06:45 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (46.67%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.java
+++ 869-reordered-power-of-2/869-reordered-power-of-2.java
@@ -1,47 +1,29 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
-        String temp = Integer.toString(n);
-        int[] num = new int[temp.length()];
-        for (int i = 0; i < temp.length(); i++)
-        {
-            num[i] = temp.charAt(i) - '0';
-        }
+        int[] freqOfN = countFreq(n);
         
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        // if (num.length ==0) return ans;
-        List<Integer> l0 = new ArrayList<Integer>();
-        l0.add(num[0]);
-        ans.add(l0);
-        for (int i = 1; i< num.length; ++i){
-            List<List<Integer>> new_ans = new ArrayList<List<Integer>>(); 
-            for (int j = 0; j<=i; ++j){            
-                for (List<Integer> l : ans){
-        	        List<Integer> new_l = new ArrayList<Integer>(l);
-        	        new_l.add(j,num[i]);
-        	        new_ans.add(new_l);
-                }
-                }
-                ans = new_ans;
-        }
-        List<Integer> allper = new ArrayList<>();
-        for(List<Integer> li : ans)
+        for(int i=0;i<31;i++)
         {
-            int val=0;
-            for(int i=0;i<li.size();i++){
-                if(i==0 && li.get(i)==0) break;
-                val=val*10 + li.get(i);
-            }
-            allper.add(val);
-        }
-        for(Integer i : allper){
-            if(power(i)) return true;
+            int powerof2 = (int)Math.pow(2,i);
+            int[] freqOfDigits = countFreq(powerof2);
+            if(checkArray(freqOfN , freqOfDigits)) return true;
         }
         return false;
-
     }
-    public boolean power(int n){
-        if((n!=0) && (n&(n-1))==0) return true;
-        // ((x & (x - 1)) == 0)
-        return false;
+    
+    private boolean checkArray(int[] a1, int[] a2){
+        for(int i=0;i<10;i++)
+        {
+            if(a1[i]!=a2[i]) return false;
+        }
+        return true;
+    }
+    private int[] countFreq(int n){
+        int[] freq = new int[10];
+        while(n!=0){
+            freq[n%10]++;
+            n=n/10;
+        }
+        return freq;
     }
 }
