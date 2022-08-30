dcc59ce7dc997e4290a887d1ad19461f6f94685e
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-08-30 15:23:09 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (38.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,18 +1,41 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int m = matrix.length;
-        int[][] res = new int[m][m];
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<m;j++){
-                res[i][j] = matrix[m-1-j][i];
+        
+//         just reversed the rows
+        for(int col=0;col<m;col++){
+            for(int row=0;row<m/2;row++){
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[m-1-row][col];
+                matrix[m-1-row][col]  = temp;
             }
         }
+        
+        
+//         single transpose
         for(int i=0;i<m;i++){
-            for(int j=0;j<m;j++){
-                matrix[i][j]=res[i][j];
+            for(int j=0;j<i;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
+        
+        
+        // int[][] res = new int[m][m];
+        
+        
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<m;j++){
+        //         res[i][j] = matrix[m-1-j][i];
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<m;j++){
+        //         matrix[i][j]=res[i][j];
+        //     }
+        // }
     
         return ;
     }
