3c0e584cd927a0f97d3b0d4cf7d6d7c187473949
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-09-05 12:12:12 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (69.35%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -11,16 +11,35 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
-        if(head==null) return null;
         
-        ListNode curr= head;
-        while(curr!=null){
-            if(set.contains(curr)) return curr;
-            set.add(curr);
-            curr=curr.next;
+        ListNode slow =head;
+        ListNode fast=head;
+        
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow = slow.next;
+            if(slow==fast) break;
         }
-        return null;
+        if(fast==null ||fast.next==null) return null;
+        
+        fast=head;
+        if(slow==head) return slow;
+        while(fast!=slow){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+            
+//         Set<ListNode> set = new HashSet<>();
+//         if(head==null) return null;
+        
+//         ListNode curr= head;
+//         while(curr!=null){
+//             if(set.contains(curr)) return curr;
+//             set.add(curr);
+//             curr=curr.next;
+//         }
+//         return null;
             
     }
 }
