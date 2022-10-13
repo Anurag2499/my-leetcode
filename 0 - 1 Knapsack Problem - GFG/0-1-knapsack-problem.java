b0769da8c80db63b2c9eacaa6b157c90fcf58911
Anurag Singh Dangi <82889022+Anurag2499@users.noreply.github.com>
2022-10-13 16:54:53 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -51,27 +51,26 @@ class Solution
     
     public int knapSack(int W, int wt[], int val[], int N) 
     {
-        int[][] t = new int[W+1][N+1];
-        for(int i=0;i<W+1;i++){
-            for(int j=0;j<N+1;j++){
-                t[i][j]=-1;
-            }       
+        int[][] t = new int[N+1][W+1];
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<W;j++)
+            {
+                if(i==0|| j==0) t[i][j]=0;
+            }
         }
-        return maxp(wt,val,0,W,N,t);
-    } 
-    private int maxp(int[] wt, int[] pro ,int ci, int cap, int n ,int[][] t)
-    {
-        if(ci==n || cap==0) return 0;
         
-        if(t[cap][ci]!=-1) return t[cap][ci];
-        
-        int taken=0;
-        if(wt[ci]<=cap){
-            taken =pro[ci] + maxp(wt,pro,ci+1,cap-wt[ci],n,t);
+        for(int i=1;i<N+1;i++){
+            for(int j=1;j<W+1;j++){
+                if(wt[i-1]<=j){
+                    t[i][j] = Math.max(val[i-1] + t[i-1][j-wt[i-1]],t[i-1][j]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
         }
-        int notTaken = maxp(wt,pro,ci+1,cap,n,t);
-        
-        return t[cap][ci] = Math.max(taken,notTaken);
+        return t[N][W];
     }
 }
 
